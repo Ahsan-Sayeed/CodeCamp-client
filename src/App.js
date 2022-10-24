6ec78c2d2515ff8782342f23a2130ae4bce22ce4
ahsan sayeed <ahsansayeedsaad@gmail.com>
2022-10-24 22:16:24 +0100

    Login form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
-
+import Footer from './Components/Footer/Footer';
+import Login from './Pages/Login/Login'
 function App() {
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
+      <Login></Login>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { BsGoogle, BsGithub } from "react-icons/bs";
+import Button from "react-bootstrap/esm/Button";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Form from "react-bootstrap/Form";
+
+function Login() {
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
+
+  return (
+    <Container fluid className="p-3 my-5">
+      <Row>
+        <Col xs={"10"} md={"6"}>
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="img-fluid"
+            alt="Sample image"
+          />
+        </Col>
+
+        <Col col="4" md="6">
+          <div className="d-flex flex-row align-items-center justify-content-center">
+            <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+
+            <Button
+              className="rounded-circle d-flex align-items-center justify-content-center"
+              style={{ height: "40px", width: "40px" }}
+            >
+              <BsGoogle size={"14px"} />
+            </Button>
+
+            <Button
+              className="rounded-circle d-flex align-items-center justify-content-center mx-2"
+              style={{ height: "40px", width: "40px" }}
+            >
+              <BsGithub size={"14px"} />
+            </Button>
+          </div>
+
+          <Row>
+            <Col>
+              <hr />
+            </Col>
+            <Col xs="auto">OR</Col>
+            <Col>
+              <hr />
+            </Col>
+          </Row>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter email" required />
+              <Form.Text className="text-muted d-flex">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+              <Form.Text className="text-muted d-flex">
+                Wrong password
+              </Form.Text>
+            </Form.Group>
+
+            <div className="d-flex justify-content-between mb-4">
+              <Form.Group className="mb-3">
+                <Form.Check
+                  type="checkbox"
+                  id="disabledFieldsetCheck"
+                  label="Can't check this"
+                />
+              </Form.Group>
+              <a href="!#">Forgot password?</a>
+            </div>
+
+            <div className="text-center text-md-start mt-4 pt-2">
+              <Button type="submit">Submit</Button>
+
+              <p className="small fw-bold mt-2 pt-1 mb-2">
+                Don't have an account?{" "}
+                <a href="#!" className="link-danger">
+                  Register
+                </a>
+              </p>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Login;
